e527cbffeca8ac998f01c1630c840a2403f92a4e
서민재 <hey_minj@naver.com>
2024-11-10 23:25:28 +0900

    search 삭제

--- search/forms.py
+++
@@ -1,4 +0,0 @@
-from django import forms
-
-class SearchForm(forms.Form):
-    search_term = forms.CharField(label='검색어', max_length=100)
--- search/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from search import views
-
-urlpatterns = [
-    path('', views.index),
-    path('search/', views.search_view),
-]
